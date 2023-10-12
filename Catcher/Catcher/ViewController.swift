1fa9cc722731c9ec537afcdd12f0926cc0815312
김징니 <105254025+jingni1115@users.noreply.github.com>
2023-10-12 20:26:54 +0900

    Add Xcode file

---
+++ Catcher/Catcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Catcher
+//
+//  Created by 김지은 on 2023/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Catcher/CatcherUITests/CatcherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CatcherUITestsLaunchTests.swift
+//  CatcherUITests
+//
+//  Created by 김지은 on 2023/10/12.
+//
+
+import XCTest
+
+final class CatcherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
