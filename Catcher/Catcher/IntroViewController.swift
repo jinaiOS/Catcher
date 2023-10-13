8e65720ae12fa3c93bf555be252e869c7e0837bc
김징니 <kj227777@naver.com>
2023-10-13 15:26:09 +0900

    [FEAT] : ⚙️ Add IntroViewController

---
+++ Catcher/Catcher/IntroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IntroViewController.swift
+//  Catcher
+//
+//  Created by 김지은 on 2023/10/13.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
