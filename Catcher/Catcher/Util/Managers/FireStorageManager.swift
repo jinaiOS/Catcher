4c18c99dd4266afbd7d564a5dac5a8785e36a799
jiwook_han <jiwook.han.dev@gmail.com>
2023-10-22 11:51:32 +0900

    [FEAT] : ⚙️ 채팅 시 이미지 저장이 가능하도록 FireStorageManager에 기능 추가

--- Catcher/Catcher/Util/Managers/FireStorageManager.swift
+++ Catcher/Catcher/Util/Managers/FireStorageManager.swift
@@ -5,19 +5,26 @@
 //  Copyright (c) 2023 z-wook. All right reserved.
 //
 
-import Foundation
+import UIKit
 import FirebaseStorage
 
 enum FireStorageError: Error {
     case noMetaData
+    case noResult
+}
+
+enum ImageError: Error {
+    case cantConvertJpeg
 }
 
 final class FireStorageManager {
     static let shared = FireStorageManager()
     private let storage = Storage.storage()
     
+    private let chatImagePath = "chat"
     private let profileImagePath = "profile"
     private let fileSize: Int64 = 1024
+    private let compressionQuality: CGFloat = 0.7
     private var uid: String?
     
     private init() {
@@ -26,33 +33,106 @@ final class FireStorageManager {
 }
 
 extension FireStorageManager {
-    func setProfileData(imageData: Data, compeltion: @escaping (Error?) -> Void) {
-        guard let uid = uid else { return }
-        let storageRef = storage.reference()
-        let imagesRef = storageRef.child(profileImagePath)
-        let spaceRef = imagesRef.child(uid)
-        
-        spaceRef.putData(imageData, metadata: nil) { (metadata, error) in
+    func setChatImageData(chatID: String, imageID: String, image: UIImage, completion: @escaping (Error?) -> Void) {
+        let data = makeImageData(image: image, completion: completion)
+        let spaceRef = makeChatRef(chatID: chatID, imageID: imageID)
+        putData(spaceRef: spaceRef, imageData: data, completion: completion)
+    }
+    
+    func fetchChatImageData(chatID: String, imageID: String, completion: @escaping (Data?, Error?) -> Void) {
+        let spaceRef = makeChatRef(chatID: chatID, imageID: imageID)
+        fetchData(spaceRef: spaceRef, completion: completion)
+    }
+    
+    func allChatImage(chatID: String, completion: @escaping (_ imageList: [String]?, Error?) -> Void) {
+        let path = "\(chatImagePath)/\(chatID)"
+        let storageReference = storage.reference().child(path)
+        storageReference.listAll { (result, error) in
             if let error = error {
-                compeltion(error)
+                completion(nil, error)
                 return
             }
-            guard let metadata = metadata else {
-                compeltion(FireStorageError.noMetaData)
+            guard let result = result else {
+                completion(nil, FireStorageError.noResult)
                 return
             }
-            print("metaData: \(metadata)")
-            compeltion(nil)
+            let imageList = result.items.map { $0.name }
+            completion(imageList, nil)
         }
     }
     
+    func deleteChatImageData(chatID: String, imageID: String, completion: @escaping (Error?) -> Void) {
+        let spaceRef = makeChatRef(chatID: chatID, imageID: imageID)
+        deleteData(spaceRef: spaceRef, completion: completion)
+    }
+}
+
+extension FireStorageManager {
+    func setProfileData(image: UIImage, completion: @escaping (Error?) -> Void) {
+        let data = makeImageData(image: image, completion: completion)
+        let spaceRef = makeProfileRef
+        putData(spaceRef: spaceRef, imageData: data, completion: completion)
+    }
+    
     func fetchProfileData(completion: @escaping (Data?, Error?) -> Void) {
-        guard let uid = uid else { return }
+        let spaceRef = makeProfileRef
+        fetchData(spaceRef: spaceRef, completion: completion)
+    }
+    
+    func deleteProfileData(completion: @escaping (Error?) -> Void) {
+        let spaceRef = makeProfileRef
+        deleteData(spaceRef: spaceRef, completion: completion)
+    }
+}
+
+private extension FireStorageManager {
+    func makeChatRef(chatID: String, imageID: String) -> StorageReference? {
         let storageRef = storage.reference()
-        let imagesRef = storageRef.child(profileImagePath)
-        let spaceRef = imagesRef.child(uid)
+        let pathRef = storageRef.child(chatImagePath)
+        let spaceRef = pathRef.child(chatID)
+        let imageRef = spaceRef.child(imageID)
         
-        spaceRef.getData(maxSize: 1 * fileSize * fileSize) { data, error in
+        return imageRef
+    }
+    
+    var makeProfileRef: StorageReference? {
+        guard let uid = uid else { return nil }
+        let storageRef = storage.reference()
+        let pathRef = storageRef.child(profileImagePath)
+        let spaceRef = pathRef.child(uid)
+        
+        return spaceRef
+    }
+}
+
+private extension FireStorageManager {
+    func makeImageData(image: UIImage, completion: @escaping (Error?) -> Void) -> Data {
+        guard let data = image.jpegData(compressionQuality: compressionQuality) else {
+            completion(ImageError.cantConvertJpeg)
+            return Data()
+        }
+        return data
+    }
+}
+
+private extension FireStorageManager {
+    func putData(spaceRef: StorageReference?, imageData: Data, completion: @escaping (Error?) -> Void) {
+        spaceRef?.putData(imageData, metadata: nil) { (metadata, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+            guard let metadata = metadata else {
+                completion(FireStorageError.noMetaData)
+                return
+            }
+            print("metaData: \(metadata)")
+            completion(nil)
+        }
+    }
+    
+    func fetchData(spaceRef: StorageReference?, completion: @escaping (Data?, Error?) -> Void) {
+        spaceRef?.getData(maxSize: 1 * fileSize * fileSize) { data, error in
             if let error = error {
                 completion(nil, error)
                 return
@@ -61,13 +141,8 @@ extension FireStorageManager {
         }
     }
     
-    func deleteProfileData(completion: @escaping (Error?) -> Void) {
-        guard let uid = uid else { return }
-        let storageRef = storage.reference()
-        let imagesRef = storageRef.child(profileImagePath)
-        let spaceRef = imagesRef.child(uid)
-        
-        spaceRef.delete { error in
+    func deleteData(spaceRef: StorageReference?, completion: @escaping (Error?) -> Void) {
+        spaceRef?.delete { error in
             if let error = error {
                 completion(error)
                 return
