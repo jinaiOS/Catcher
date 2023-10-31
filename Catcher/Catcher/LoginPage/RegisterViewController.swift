f0e10a90c96a31508c6d6d0d6a4034e0fdc4b65d
JeongKiKi <jkh001301@gmail.com>
2023-10-31 12:00:01 +0900

    [FEAT] : ⚙️ 회원가입 닉네임 중복확인

--- Catcher/Catcher/LoginPage/RegisterViewController.swift
+++ Catcher/Catcher/LoginPage/RegisterViewController.swift
@@ -12,9 +12,9 @@ import UIKit
 final class RegisterViewController: BaseViewController {
     private let registerView = RegisterView()
     private let fireManager = FirebaseManager()
-    
+
     /** @brief 공통 헤더 객체 */
-    var headerView : CommonHeaderView!
+    var headerView: CommonHeaderView!
 
     @objc func nextPressed() {
         guard let nickName = registerView.nicknametextfield.text, !nickName.isEmpty else {
@@ -31,14 +31,33 @@ final class RegisterViewController: BaseViewController {
             showAlert(title: "유효하지 않은 비밀번호", message: "비밀번호를 다시 확인해주세요.")
             return
         }
-//        guard let nickName = registerView.nicknametextfield.text else { return }
-//        guard let email = registerView.emailtextfield.text else { return }
-//        guard let password = registerView.passwordtextfield.text else { return }
-        let vcInfo = InfoViewController()
-        vcInfo.newUserEmail = email
-        vcInfo.newUserPassword = password
-        vcInfo.newUserNickName = nickName
-        navigationController?.pushViewController(vcInfo, animated: true)
+        
+        //        guard let nickName = registerView.nicknametextfield.text else { return }
+        //        guard let email = registerView.emailtextfield.text else { return }
+        //        guard let password = registerView.passwordtextfield.text else { return }
+
+        async {
+            do {
+                let (isAvailable, error) = try await FireStoreManager.shared.nickNamePass(nickName: nickName)
+                if let error = error { print("Error checking nickName availability: \(error.localizedDescription)")
+                    return
+                }
+                if isAvailable == true {
+                    // 닉네임 사용 가능
+                    // 이후 회원가입 프로세스 진행
+                    let vcInfo = InfoViewController()
+                    vcInfo.newUserEmail = email
+                    vcInfo.newUserPassword = password
+                    vcInfo.newUserNickName = nickName
+                    navigationPushController(viewController: vcInfo, animated: true)
+                } else {
+                    // 닉네임 이미 사용 중
+                    showAlert(title: "닉네임 중복", message: "다른 닉네임을 사용해주세요.")
+                }
+            } catch {
+                print("Error checking nickName availability: \(error.localizedDescription)")
+            }
+        }
     }
 
     func showAlert(title: String, message: String) {
@@ -51,34 +70,32 @@ final class RegisterViewController: BaseViewController {
         present(alertController, animated: true, completion: nil)
     }
 
-
     func isValidEmail(_ email: String) -> Bool {
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
         return emailPredicate.evaluate(with: email)
     }
-    
+
     func setHeaderView() {
-        headerView = CommonHeaderView.init(frame: CGRect.init(x: 0, y: Common.kStatusbarHeight, width: Common.SCREEN_WIDTH(), height: 50))
-        
+        headerView = CommonHeaderView(frame: CGRect(x: 0, y: Common.kStatusbarHeight, width: Common.SCREEN_WIDTH(), height: 50))
+
         view.addSubview(headerView)
-        
+
         headerView.lblTitle.text = "회원가입"
     }
-    
+
     /**
      @brief backButton을 눌렀을때 들어오는 이벤트
-     
+
      @param sender 버튼 객체
      */
-    @objc func backButtonTouched(sender : UIButton)
-    {
-        navigationPopViewController(animated: true) { () -> (Void) in }
+    @objc func backButtonTouched(sender: UIButton) {
+        navigationPopViewController(animated: true) { () in }
     }
 
     override func loadView() {
         super.loadView()
         view.addSubview(registerView)
-        
+
         registerView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaInsets).offset(80)
             $0.leading.bottom.trailing.equalToSuperview()
@@ -87,12 +104,12 @@ final class RegisterViewController: BaseViewController {
 
     override func viewDidLoad() {
         registerView.nextButton.addTarget(self, action: #selector(nextPressed), for: .touchUpInside)
-        
-        let tapGesture : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        //gesture의 이벤트가 끝나도 뒤에 이벤트를 View로 전달
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        // gesture의 이벤트가 끝나도 뒤에 이벤트를 View로 전달
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
-        
+
         setHeaderView()
     }
 }
