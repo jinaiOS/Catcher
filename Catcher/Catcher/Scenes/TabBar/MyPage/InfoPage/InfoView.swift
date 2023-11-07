8c158969edf5a5f3051b4bc140f888b6ea2ff713
JeongKiKi <jkh001301@gmail.com>
2023-11-07 22:40:17 +0900

    [FIX] : 🔧  프로필 자기소개 추가 (체형, 음주, 흡연, 학력 삭제)

--- Catcher/Catcher/Scenes/TabBar/MyPage/InfoPage/InfoView.swift
+++ Catcher/Catcher/Scenes/TabBar/MyPage/InfoPage/InfoView.swift
@@ -10,25 +10,22 @@ import UIKit
 
 final class InfoView: UIView {
     private let isValidNickName: Bool
-    var selectedBodyButton: UIButton?
-    var selectedDrinkButton: UIButton?
-    var selectedSmokeButton: UIButton?
-    
+
     lazy var nickNameTextField: CustomTextField = {
         let textField = CustomTextField()
         return textField
     }()
-    
+
     lazy var regionTextField: CustomTextField = {
         let textField = CustomTextField()
         return textField
     }()
-    
-    lazy var educationTextField: CustomTextField = {
+
+    lazy var mbtiTextField: CustomTextField = {
         let textField = CustomTextField()
         return textField
     }()
-    
+
     lazy var heightTextField: CustomTextField = {
         let textField = CustomTextField()
         textField.tf.keyboardType = .numberPad
@@ -39,124 +36,26 @@ final class InfoView: UIView {
         let textField = CustomTextField()
         return textField
     }()
-    
-    lazy var thinBodyBtn: UIButton = bodyButton(title: "마름")
-    
-    lazy var nomalBodyBtn: UIButton = bodyButton(title: "평범함")
-    
-    lazy var chubbyBodyBtn: UIButton = bodyButton(title: "통통함")
-    
-    lazy var fatBodyBtn: UIButton = bodyButton(title: "뚱뚱함")
-    
-    lazy var drinkingNoBtn: UIButton = drinkButton(title: "안 마심")
-    
-    lazy var drinkingTwiceBtn: UIButton = drinkButton(title: "주 1~2회")
-    
-    lazy var drinkingOftenBtn: UIButton = drinkButton(title: "주 3~5회")
-    
-    lazy var drinkingDailyBtn: UIButton = drinkButton(title: "그 이상")
-    
-    lazy var smokingBtn: UIButton = smokeButton(title: "흡연")
-    
-    lazy var noSmokingBtn: UIButton = smokeButton(title: "비흡연")
-    
-    @objc func bodyButtonTapped(_ sender: UIButton) {
-        sender.isSelected = !sender.isSelected
-        if let selectedBodyButton = selectedBodyButton {
-            selectedBodyButton.isSelected = false
-            selectedBodyButton.backgroundColor = .systemGray4
-            selectedBodyButton.setTitleColor(.darkGray, for: .normal)
-        }
-        sender.isSelected = true
-        sender.setTitleColor(.white, for: .normal)
-        sender.backgroundColor = ThemeColor.primary
 
-        selectedBodyButton = sender // 선택된 버튼 업데이트
-    }
-
-    @objc func drinkButtonTapped(_ sender: UIButton) {
-        sender.isSelected = !sender.isSelected
-        if let selectedDrinkButton = selectedDrinkButton {
-            selectedDrinkButton.isSelected = false
-            selectedDrinkButton.backgroundColor = .systemGray4
-            selectedDrinkButton.setTitleColor(.darkGray, for: .normal)
-        }
-        sender.isSelected = true
-        sender.setTitleColor(.white, for: .normal)
-        sender.backgroundColor = ThemeColor.primary
-
-        selectedDrinkButton = sender // 선택된 버튼 업데이트
-    }
-    
-    @objc func smokeButtonTapped(_ sender: UIButton) {
-        sender.isSelected = !sender.isSelected
-        if let selectedSmokeButton = selectedSmokeButton {
-            selectedSmokeButton.isSelected = false
-            selectedSmokeButton.backgroundColor = .systemGray4
-            selectedSmokeButton.setTitleColor(.darkGray, for: .normal)
-        }
-        sender.isSelected = true
-        sender.setTitleColor(.white, for: .normal)
-        sender.backgroundColor = ThemeColor.primary
-
-        selectedSmokeButton = sender // 선택된 버튼 업데이트
-    }
-    
-    private lazy var bodyHstack: UIStackView = {
-        let view = UIStackView()
-        view.axis = .horizontal
-        view.alignment = .fill
-        view.distribution = .fillEqually
-        view.spacing = AppConstraint.stackViewSpacing
-        
-        [thinBodyBtn, nomalBodyBtn, chubbyBodyBtn, fatBodyBtn].forEach {
-            view.addArrangedSubview($0)
-        }
-        return view
+    lazy var introduceTextField: CustomTextField = {
+        let textField = CustomTextField()
+        return textField
     }()
-    
-    private lazy var drinkingHstack: UIStackView = {
-        let view = UIStackView()
-        view.axis = .horizontal
-        view.alignment = .fill
-        view.distribution = .fillEqually
-        view.spacing = AppConstraint.stackViewSpacing
-        
-        [drinkingNoBtn, drinkingTwiceBtn, drinkingOftenBtn, drinkingDailyBtn].forEach {
-            view.addArrangedSubview($0)
-        }
-        return view
-    }()
-    
-    private lazy var smokingHstack: UIStackView = {
-        let view = UIStackView()
-        view.axis = .horizontal
-        view.alignment = .fill
-        view.distribution = .fillEqually
-        view.spacing = AppConstraint.stackViewSpacing
-        
-        [smokingBtn, noSmokingBtn].forEach {
-            view.addArrangedSubview($0)
-        }
-        return view
-    }()
-    
+
     private lazy var vStack: UIStackView = {
         let view = UIStackView()
         view.axis = .vertical
         view.distribution = .fillProportionally
         view.spacing = AppConstraint.stackViewSpacing
-        
+
         if isValidNickName {
             view.addArrangedSubview(nickNameTextField)
         }
         [regionTextField,
          birthTextField,
-         educationTextField,
+         mbtiTextField,
          heightTextField,
-         makeLabel(text: "체형"), bodyHstack, separateView,
-         makeLabel(text: "음주"), drinkingHstack, separateView,
-         makeLabel(text: "흡연"), smokingHstack].forEach {
+         introduceTextField].forEach {
             view.addArrangedSubview($0)
         }
         return view
@@ -184,14 +83,14 @@ final class InfoView: UIView {
         button.layer.cornerRadius = 15
         return button
     }()
-    
+
     init(isValidNickName: Bool = false) {
         self.isValidNickName = isValidNickName
         super.init(frame: .zero)
         backgroundColor = ThemeColor.backGroundColor
         setLayout()
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -199,68 +98,6 @@ final class InfoView: UIView {
 }
 
 extension InfoView {
-    func bodyButton(title: String) -> UIButton {
-        let button = UIButton(type: .custom)
-        button.setTitle(title, for: .normal)
-        button.titleLabel?.font = ThemeFont.regular(size: 15)
-        button.setTitleColor(.darkGray, for: .normal)
-        button.setTitleColor(.white, for: .selected)
-        button.backgroundColor = .systemGray4
-        button.layer.cornerRadius = 15
-        button.addTarget(self, action: #selector(bodyButtonTapped(_:)), for: .touchUpInside)
-        return button
-    }
-
-    func drinkButton(title: String) -> UIButton {
-        let button = UIButton(type: .custom)
-        button.setTitle(title, for: .normal)
-        button.titleLabel?.font = ThemeFont.regular(size: 15)
-        button.setTitleColor(.darkGray, for: .normal)
-        button.setTitleColor(.white, for: .selected)
-        button.backgroundColor = .systemGray4
-        button.layer.cornerRadius = 15
-        button.addTarget(self, action: #selector(drinkButtonTapped(_:)), for: .touchUpInside)
-        return button
-    }
-
-    func smokeButton(title: String) -> UIButton {
-        let button = UIButton(type: .custom)
-        button.setTitle(title, for: .normal)
-        button.titleLabel?.font = ThemeFont.regular(size: 15)
-        button.setTitleColor(.darkGray, for: .normal)
-        button.setTitleColor(.white, for: .selected)
-        button.backgroundColor = .systemGray4
-        button.layer.cornerRadius = 15
-        button.addTarget(self, action: #selector(smokeButtonTapped(_:)), for: .touchUpInside)
-        return button
-    }
-    
-    func makeLabel(text: String) -> UILabel {
-        return LabelFactory.makeLabel(
-            text: text,
-            font: ThemeFont.demibold(size: 18),
-            textAlignment: .left)
-    }
-    
-    func makeButton(text: String) -> UIButton {
-        return ButtonFactory.makeButton(
-            title: text,
-            titleLabelFont: ThemeFont.regular(size: 15),
-            titleColor: .darkGray,
-            backgroundColor: .systemGray4,
-            cornerRadius: 15)
-    }
-    
-    var separateView: UIView {
-        let view = UIView()
-        view.backgroundColor = ThemeColor.primary
-        
-        view.snp.makeConstraints {
-            $0.height.equalTo(1)
-        }
-        return view
-    }
-    
     func getLabelSize(text: String, font: UIFont) -> (CGFloat, CGFloat) {
         let label = UILabel()
         label.text = text
@@ -268,7 +105,7 @@ extension InfoView {
         label.sizeToFit()
         return (label.bounds.width, label.bounds.height)
     }
-    
+
     func setLayout() {
         [scrollView].forEach {
             addSubview($0)
