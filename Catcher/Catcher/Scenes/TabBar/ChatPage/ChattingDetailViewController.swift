e706c37bbb4f655f9b6d55aa7484e22119d7fafe
jingni1115 <kj227777@naver.com>
2023-11-10 05:31:36 +0900

    [FIX] : 🐛 비디오 탭 눌러서 카메라 누르면 앱 크래쉬 나는 문제 -> 사진 앨범만 갈 수 있도록 수정

--- Catcher/Catcher/Scenes/TabBar/ChatPage/ChattingDetailViewController.swift
+++ Catcher/Catcher/Scenes/TabBar/ChatPage/ChattingDetailViewController.swift
@@ -256,34 +256,13 @@ final class ChattingDetailViewController: MessagesViewController {
     }
     
     private func presentVideoInputActionsheet() {
-        let actionSheet = UIAlertController(title: "Attach Video",
-                                            message: "Where would you like to attach a video from?",
-                                            preferredStyle: .actionSheet)
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { [weak self] _ in
-            
-            let picker = UIImagePickerController()
-            picker.sourceType = .camera
-            picker.delegate = self
-            picker.mediaTypes = ["public.movie"]
-            picker.videoQuality = .typeMedium
-            picker.allowsEditing = true
-            self?.present(picker, animated: true)
-            
-        }))
-        actionSheet.addAction(UIAlertAction(title: "Library", style: .default, handler: { [weak self] _ in
-            
-            let picker = UIImagePickerController()
-            picker.sourceType = .photoLibrary
-            picker.delegate = self
-            picker.allowsEditing = true
-            picker.mediaTypes = ["public.movie"]
-            picker.videoQuality = .typeMedium
-            self?.present(picker, animated: true)
-            
-        }))
-        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        present(actionSheet, animated: true)
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.mediaTypes = ["public.movie"]
+        picker.videoQuality = .typeMedium
+        self.present(picker, animated: true)
     }
     
     private func listenForMessages(id: String, shouldScrollToBottom: Bool) {
